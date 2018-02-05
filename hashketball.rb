7d1ac470e03e8e44f684e7317d94c119636bc9cf
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 19:13:36 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,6 +34,26 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
+        },
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        "Jason Terry" => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
         }
       }
     },
