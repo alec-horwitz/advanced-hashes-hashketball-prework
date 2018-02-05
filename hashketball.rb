462f8e82eb2871530f677668aeee490d6beb51af
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 19:06:53 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,60 @@
 # Write your code here!
-
+def game_hash
+  hash = {
+    home: {
+      team_name: "", 
+      colors: [], 
+      players: {
+        "Alan Anderson" => {
+          number:, 
+          shoe:, 
+          points:, 
+          rebounds:, 
+          assists:, 
+          steals:, 
+          blocks:, 
+          slam_dunks:
+        },
+        "Alan Anderson" => {
+          number:, 
+          shoe:, 
+          points:, 
+          rebounds:, 
+          assists:, 
+          steals:, 
+          blocks:, 
+          slam_dunks:
+        }
+      }
+    }, 
+    away: {
+      team_name: "", 
+      colors: [], 
+      players: {
+        "Alan Anderson" => {
+          number:, 
+          shoe:, 
+          points:, 
+          rebounds:, 
+          assists:, 
+          steals:, 
+          blocks:, 
+          slam_dunks:
+        },
+        "Alan Anderson" => {
+          number:, 
+          shoe:, 
+          points:, 
+          rebounds:, 
+          assists:, 
+          steals:, 
+          blocks:, 
+          slam_dunks:
+        }
+      }
+    }
+  }
+end
 
 
 
